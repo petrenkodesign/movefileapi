37a13931f82e62ba145f92976dae4cc201c07ba6
Olexa Petrenko <petrenkodesign@gmail.com>
2023-04-27 15:12:59 +0300

    Update src and point path

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api_token')->get('/movefiles', function (Request $request) {
     $output = null;
     // $command = 'scp -C -i '.env('SERVER_SSHKEY').' '.env('SERVER_SRC_DIR').env('FILES_MASK').' '.env('SERVER_POINT_USER').'@'.env('SERVER_POINT_IP').':'.env('SERVER_POINT_DIR').' 2>&1';
-    $command = 'rsync -avP --ignore-existing -e "ssh -i '.env('SERVER_SSHKEY').'" '.env('SERVER_SRC_DIR').env('FILES_MASK').' '.env('SERVER_POINT_USER').'@'.env('SERVER_POINT_IP').':'.env('SERVER_POINT_DIR').' 2>&1';
+    $command = 'rsync -avP --ignore-existing -e "ssh -i '.env('SERVER_SSHKEY').'" '.env('SERVER_POINT_USER').'@'.env('SERVER_SRC_IP').':'.env('SERVER_SRC_DIR').env('FILES_MASK').' '.env('SERVER_POINT_DIR').' 2>&1';
     exec($command, $output);
     return response()->json($output, 200);
 });
